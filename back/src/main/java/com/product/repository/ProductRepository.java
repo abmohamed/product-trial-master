466e6a35fc8b384a2d99e69c9b6c7b48f0badba8
abmohamed <abmohamed@gmail.com>
2025-05-09 16:06:03 +0100

    Fix persisting to json file for post and patch

--- back/src/main/java/com/product/repository/ProductRepository.java
+++ back/src/main/java/com/product/repository/ProductRepository.java
@@ -7,10 +7,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.FileCopyUtils;
 
 import jakarta.annotation.PostConstruct;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -22,38 +27,39 @@ public class ProductRepository {
 
     private static final Logger logger = LoggerFactory.getLogger(ProductRepository.class);
     private List<Product> products = new ArrayList<>();
-    private final ObjectMapper objectMapper = new ObjectMapper();
-    private final AtomicLong counter = new AtomicLong();
-    private static final String PRODUCTS_JSON_PATH = "products.json";
+    private final ObjectMapper objMap = new ObjectMapper();
+    private final AtomicLong countAtom = new AtomicLong();
+    private static final String PRODUCTS_JSON_FILE = "products.json";
+    private static final String PATH_TO_PRODUCTS_JSON = "src/main/resources/";
 
     @PostConstruct
     public void init() {
-        loadProductsFromFile();
+        getProductsFromJSON();
         if (!products.isEmpty()) {
-            long maxId = products.stream().mapToLong(Product::getId).max().orElse(0L);
-            counter.set(maxId + 1);
+            long maximumId = products.stream().mapToLong(Product::getId).max().orElse(0L);
+            countAtom.set(maximumId + 1);
         } else {
             // We decided to start IDs from 1 in case the product list is empty
-            counter.set(1L); 
+            countAtom.set(1L);
         }
-        logger.info("The Product Repository is initialized with {} products. So the next ID will be {}.", products.size(), counter.get());
+        logger.info("The Product Repository is initialized with {} products. So the next ID will be {}.", products.size(), countAtom.get());
     }
 
-    private void loadProductsFromFile() {
+    private void getProductsFromJSON() {
         try {
-            ClassPathResource resource = new ClassPathResource(PRODUCTS_JSON_PATH);
-            if (!resource.exists()) {
-                logger.warn("The products file was not found in the classpath: {}", PRODUCTS_JSON_PATH);
+            ClassPathResource resourcePath = new ClassPathResource(PRODUCTS_JSON_FILE);
+            if (!resourcePath.exists()) {
+                logger.warn("The products file was not found in the classpath: {}", PRODUCTS_JSON_FILE);
                 // Start the initialization with an empty list otherwise we should handle as an error
                 this.products = new ArrayList<>();
                 return;
             }
-            try (InputStream inputStream = resource.getInputStream()) {
-                this.products = objectMapper.readValue(inputStream, new TypeReference<List<Product>>() {});
-                logger.info("WE successfully loaded {} products from the predefined classpath: {}", products.size(), PRODUCTS_JSON_PATH);
+            try (InputStream inputStream = resourcePath.getInputStream()) {
+                this.products = objMap.readValue(inputStream, new TypeReference<List<Product>>() {});
+                logger.info("WE successfully loaded {} products from the predefined classpath: {}", products.size(), PRODUCTS_JSON_FILE);
             }
         } catch (IOException e) {
-            logger.error("There is an error when loading products from the predefined classpath: {}", PRODUCTS_JSON_PATH, e);
+            logger.error("There is an error when loading products from the predefined classpath: {}", PRODUCTS_JSON_FILE, e);
             // Load an empty list in case of error
             this.products = new ArrayList<>();
         }
@@ -69,28 +75,28 @@ public class ProductRepository {
 
     public Product save(Product product) {
         if (product.getId() == null) {
-            product.setId(counter.getAndIncrement());
+            product.setId(countAtom.getAndIncrement());
             product.setCreatedAt(System.currentTimeMillis());
             product.setUpdatedAt(System.currentTimeMillis());
             products.add(product);
         } else {
-            Optional<Product> existingProductOpt = findById(product.getId());
-            if (existingProductOpt.isPresent()) {
-                Product existingProduct = existingProductOpt.get();
-                product.setCreatedAt(existingProduct.getCreatedAt());
+            Optional<Product> findByIdexistingProduct = findById(product.getId());
+            if (findByIdexistingProduct.isPresent()) {
+                Product verifiedExistingProduct = findByIdexistingProduct.get();
+                product.setCreatedAt(verifiedExistingProduct.getCreatedAt());
                 product.setUpdatedAt(System.currentTimeMillis());
-                products.remove(existingProduct);
+                products.remove(verifiedExistingProduct);
                 products.add(product);
             } else {
                 // In case we did not find the product by it's id, then we will assign a new id to avoid any conflicts
-                product.setId(counter.getAndIncrement());
+                product.setId(countAtom.getAndIncrement());
                 product.setCreatedAt(System.currentTimeMillis());
                 product.setUpdatedAt(System.currentTimeMillis());
                 products.add(product);
             }
         }
         // Save the product to the json file
-        persistProductsToFile(); 
+        persistProductsToJSONFile(); 
         return product;
     }
 
@@ -99,17 +105,20 @@ public class ProductRepository {
                            .filter(product -> !product.getId().equals(id))
                            .collect(Collectors.toList());
         // Delete the product to the json file
-        persistProductsToFile(); 
+        persistProductsToJSONFile();
     }
 
-
-    private void persistProductsToFile() {
+    private void persistProductsToJSONFile() {
         try {
-            ClassPathResource resource = new ClassPathResource(PRODUCTS_JSON_PATH);
-            objectMapper.writerWithDefaultPrettyPrinter().writeValue(resource.getFile(), products);
-            logger.info("Successfully persisted products to {}", PRODUCTS_JSON_PATH);
+            // Get the json file using path to find the file path
+            File jsonFile = new File(PATH_TO_PRODUCTS_JSON + PRODUCTS_JSON_FILE);
+
+            try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(jsonFile), StandardCharsets.UTF_8)) {
+                objMap.writerWithDefaultPrettyPrinter().writeValue(writer, products);
+                logger.info("Successfully persisted {} products to {}", products.size(), jsonFile.getAbsolutePath());
+            }
         } catch (IOException e) {
-            logger.error("Error persisting products to {}", PRODUCTS_JSON_PATH, e);
+            logger.error("Error persisting products to {}", PRODUCTS_JSON_FILE, e);
         }
     }
 }
